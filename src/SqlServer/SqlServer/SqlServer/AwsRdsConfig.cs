4d959a7bbfede07aeb16b05d5bd9e5d45f1910c5
Mario Campos <mario-campos@github.com>
2020-11-25 13:29:46 -0600

    Introduce SqlServer project with SQLi vulns

---
+++ src/SqlServer/SqlServer/SqlServer/AwsRdsConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ServiceStack.Configuration;
+
+namespace SqlServer
+{
+    public class AwsRdsConfig
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                var connString = ConfigUtils.GetNullableAppSetting("ConnectionString")
+                   ?? Environment.GetEnvironmentVariable("ConnectionString");
+
+                if (string.IsNullOrEmpty(connString))
+                    throw new ArgumentException("ConnectionString must be defined in App.config or Environment Variable");
+
+                return connString;
+            }
+        }
+    }
+}
